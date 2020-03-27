210df0c34e98016144323914e6fa4c6a5d368099
Abhay Raj Singh <abhayraja4@gmail.com>
2020-03-27 07:21:10 +0530

    Adding documentation

--- deploy.py
+++ deploy.py
@@ -1,9 +1,14 @@
 # Using flask to make an api 
 # import necessary libraries and functions 
 from flask import Flask, jsonify, request
+# documentation using swagger
+from flask.ext.restplus import Api, Resource, fields
 
 # creating a Flask app 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Mock API',
+    description='A simple and secure mock-server API defining the basic structure and API contracts for developers'
+)
 
 # on the terminal type: curl http://127.0.0.1:5000/ 
 # returns hello world when we use GET. 
@@ -32,11 +37,22 @@ def matrix():
         return jsonify({'status': status})
 
 
+@api.doc(responses={404: 'Todo not found'}, params={'todo_id': 'The Todo ID'})
 @app.route('/matrix', methods = ['POST'])
 def createCompatibility():
         password = request.args.get('password')
         page = request.args.get('page', default = 1, type = int)
         return jsonify({'pass': password, 'pageSizeRequested': page})
+
+
+@api.doc(responses={201: 'User has been created'}, params={'username': 'Username', 'password': 'password'})
+@app.route('/api/users', methods = ['POST'])
+def createNewUser():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    return jsonify({ 'username': username, 'password': username  }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
 
 
 # driver function 
