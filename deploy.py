14fdfbde687f8e4c475f01ef8ca7b8c32bd99495
Abhay Raj Singh <abhayraja4@gmail.com>
2020-03-27 11:30:09 +0530

    Todo app

--- deploy.py
+++ deploy.py
@@ -1,33 +1,12 @@
-# Using flask to make an api 
-# import necessary libraries and functions 
-from flask import Flask, jsonify, request
-from flask_restplus import Api, Resource, fields
+from flask import Flask
+from flask.ext.restplus import Api, Resource, fields
 
-
-# creating a Flask app 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Todo API',
     description='A simple TODO API extracted from the original flask-restful example'
 )
-ns = api.namespace('squares', description='square operations')
 
-# on the terminal type: curl http://127.0.0.1:5000/ 
-# returns hello world when we use GET. 
-# returns the data that we send when we use POST. 
-@api.doc(responses={404: 'Todo not found'}, params={'todo_id': 'The Todo ID'})
-@app.route('/', methods = ['GET', 'POST'])
-def home():
-        status = "up"
-        return jsonify({'status': status})
-
-
-# A simple function to calculate the square of a number 
-# the number to be squared is sent in the URL when we use GET 
-# on the terminal type: curl http://127.0.0.1:5000 / home / 10 
-# this returns 100 (square of 10) 
-@app.route('/square/<int:num>', methods = ['GET'])
-def disp(num):
-        return jsonify({'data': num**2}), 201
+ns = api.namespace('todos', description='TODO operations')
 
 TODOS = {
     'todo1': {'task': 'build an API'},
@@ -44,7 +23,11 @@ def abort_if_todo_doesnt_exist(todo_id):
     if todo_id not in TODOS:
         api.abort(404, "Todo {} doesn't exist".format(todo_id))
 
-@ns.route('/todo/<string:todo_id>')
+parser = api.parser()
+parser.add_argument('task', type=str, required=True, help='The task details')
+
+
+@ns.route('/<string:todo_id>')
 @api.doc(responses={404: 'Todo not found'}, params={'todo_id': 'The Todo ID'})
 class Todo(Resource):
     '''Show a single todo item and lets you delete them'''
@@ -70,31 +53,24 @@ class Todo(Resource):
         TODOS[todo_id] = task
         return task, 201
 
-"""
-Function returns the dummy matrix in response
-"""
-@api.route('/matrix', methods = ['GET'])
-def matrix():
-        status = "up"
-        return jsonify({'status': status})
+
+@ns.route('/')
+class TodoList(Resource):
+    '''Shows a list of all todos, and lets you POST to add new tasks'''
+    @api.marshal_with(todo_fields, as_list=True)
+    def get(self):
+        '''List all todos'''
+        return TODOS
+
+    @api.doc(parser=parser)
+    @api.marshal_with(todo_fields)
+    def post(self):
+        '''Ceate a todo'''
+        args = parser.parse_args()
+        todo_id = 'todo%d' % (len(TODOS) + 1)
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
 
 
-@app.route('/matrix', methods = ['POST'])
-def createCompatibility():
-        password = request.args.get('password')
-        page = request.args.get('page', default = 1, type = int)
-        return jsonify({'pass': password, 'pageSizeRequested': page})
-
-
-@app.route('/api/users', methods = ['POST'])
-def createNewUser():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        abort(400) # missing arguments
-    return jsonify({ 'username': username, 'password': username  }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
-
-
-# driver function 
 if __name__ == '__main__':
-        app.run(debug = True)
+    app.run(debug=True)
