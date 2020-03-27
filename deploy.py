16862865c3c271848141ccc66ed56dde22c4271a
Abhay Raj Singh <abhayraja4@gmail.com>
2020-03-27 11:24:53 +0530

    Docs as in documentation

--- deploy.py
+++ deploy.py
@@ -25,10 +25,50 @@ def home():
 # the number to be squared is sent in the URL when we use GET 
 # on the terminal type: curl http://127.0.0.1:5000 / home / 10 
 # this returns 100 (square of 10) 
-@ns.route('/square/<int:num>', methods = ['GET'])
+@app.route('/square/<int:num>', methods = ['GET'])
 def disp(num):
         return jsonify({'data': num**2}), 201
 
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+todo_fields = api.model('Todo', {
+    'task': fields.String(required=True, description='The task details')
+})
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        api.abort(404, "Todo {} doesn't exist".format(todo_id))
+
+@ns.route('/todo/<string:todo_id>')
+@api.doc(responses={404: 'Todo not found'}, params={'todo_id': 'The Todo ID'})
+class Todo(Resource):
+    '''Show a single todo item and lets you delete them'''
+    @api.doc(notes='todo_id should be in {0}'.format(', '.join(TODOS.keys())))
+    @api.marshal_with(todo_fields)
+    def get(self, todo_id):
+        '''Fetch a given resource'''
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        '''Delete a given resource'''
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    @api.doc(parser=parser)
+    @api.marshal_with(todo_fields)
+    def put(self, todo_id):
+        '''Update a given resource'''
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
 
 """
 Function returns the dummy matrix in response
